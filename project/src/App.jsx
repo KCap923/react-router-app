83dc1390a79c84c95f2d7855a2dc97d86a6c8586
Krystle Capuano <krystlecapuano@gmail.com>
2024-06-14 16:27:44 -0400

    Routes connected

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,10 +1,11 @@
-// import "../src/main/App.css";
+import "./index.css";
 
 // Import Route and our components
 import { Route, Routes } from "react-router-dom";
 import Currencies from "../src/components/Currencies";
 import Main from "../src/components/Main";
 import Price from "../src/pages/Price";
+import Nav from "../src/components/Nav";
 
 export default function App () {
   // We will use the Route component, path attribute, and element attribute to specify each route.
